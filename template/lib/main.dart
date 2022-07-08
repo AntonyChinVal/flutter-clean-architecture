1193dc4a6d28c999d4aa76203fe7c761cd26c831
Antony David Chinchay Valdivia <antonychinchay@bcp.com.pe>
2022-07-08 03:27:25 -0500

    Some

--- template/lib/main.dart
+++ template/lib/main.dart
@@ -2,6 +2,9 @@ import 'configuration/app_configuration/app_configuration.dart';
 import 'configuration/injection_configuration/injection_container.dart';
 
 void main() async {
+
+
+  
   configureInjection();
   configureApp();
 }
